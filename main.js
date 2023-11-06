fd22c1cc888ab1520ea324c07250ba239143747f
RomanKosmyna <roma.kosmyna@gmail.com>
2023-11-06 21:45:17 +0200

    first commit

---
+++ main.js
@@ -0,0 +1,39 @@
+const beforeSubscribeEl = document.querySelector(".before-subscribe-container");
+const afterSubscribeEl = document.querySelector(".after-subscribe-container");
+let userEmail = "";
+
+const handleSubscribe = (e) => {
+    e.preventDefault();
+
+    const emailPattern = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/;
+
+    const inputEl = document.querySelector(".form-input");
+    const errorEl = document.querySelector("#error");
+
+    const userEmailEl = document.querySelector("#user-email");
+
+    if (!emailPattern.test(inputEl.value)) {
+        errorEl.style.display = "block";
+        inputEl.style.background = "hsl(4, 100%, 87%)";
+        inputEl.style.border = "1px solid hsl(4, 100%, 67%)";
+        inputEl.style.color = "hsl(4, 100%, 67%)";
+
+        return;
+    } else {
+        errorEl.style.display = "none";
+        inputEl.style.background = "none";
+        inputEl.style.border = "1px solid hsl(231, 7%, 60%)";
+        inputEl.style.color = "hsl(234, 29%, 20%";
+
+        beforeSubscribeEl.style.display = "none";
+        afterSubscribeEl.style.display = "flex";
+        
+        userEmail = inputEl.value;
+        userEmailEl.textContent = userEmail;
+    }
+};
+
+const dismissMessage = () => {
+    beforeSubscribeEl.style.display = "block";
+    afterSubscribeEl.style.display = "none";
+};
